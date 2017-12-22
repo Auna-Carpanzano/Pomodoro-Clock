2478d20b110765166fba8ae20a68fc49f34eeac2
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-22 16:25:42 -0700

    Store location of session number in sessionCount variable

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
   var alarm = $("#alarm")[0];
   var breakCount = parseInt($("#breakNum").html());
+  var sessionCount = parseInt($("#sessionNum").html());
 }); //End document ready function
