6282c432b9ac3276b0b50a7c21fbb742913378e3
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-22 17:35:10 -0700

    Decrease breakCount by one each second due to breakTime function

--- scripts.js
+++ scripts.js
@@ -68,6 +68,7 @@ $(document).ready(function () {
         $("#timeType").show();
         $("#timeType").html("Break time: ");
         $("#breakNum").show();
+        breakCount = breakCount - 1;
         //Format breakCount to minutes:seconds
         if (breakCount%60 >= 10) {
           $("#breakNum").html(Math.floor(breakCount/60)+":"+breakCount%60);
